47ee5d725b3f00f4b68a489c4a19ae1e84321a2a
arierg03 <72164442+arierg03@users.noreply.github.com>
2023-11-06 10:20:58 +0100

    Añadido pyttsx3

--- prueba.py
+++ prueba.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
+import pyttsx3
 
 recognizer = sr.Recognizer()
+engine = pyttsx3.init()
 
 with sr.Microphone() as source:
     print("Habla algo...")
@@ -9,6 +11,10 @@ with sr.Microphone() as source:
 try:
     text = recognizer.recognize_google(audio, language="es-ES")
     print("Has dicho: " + text)
+
+    if 'hola' in text:
+        engine.say("Buenos dias")
+        engine.runAndWait()
 
     # Guarda las palabras transcritas en un archivo de texto
     with open("transcripcion.txt", "a") as file:
