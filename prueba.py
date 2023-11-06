fb7f53aa54245c39da8baf0261c34606beb3dafb
arierg03 <72164442+arierg03@users.noreply.github.com>
2023-11-06 10:51:49 +0100

    incluido pyaudio

--- prueba.py
+++ prueba.py
@@ -1,9 +1,20 @@
 import speech_recognition as sr
 import pyttsx3
+import pyaudio
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init(driverName='espeak',debug=True)
 engine.setProperty('voice', 'es')
+
+p = pyaudio.PyAudio()
+
+# Enumera los dispositivos de salida disponibles
+for i in range(p.get_device_count()):
+    device_info = p.get_device_info_by_index(i)
+    print(device_info['name'])
+
+# Configura el motor de texto a voz para usar el dispositivo de salida específico
+engine.setProperty('output', output_device_index)
 
 with sr.Microphone() as source:
     print("Habla algo...")
