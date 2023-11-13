455e31ec7df020ddabdcbc94fc4ef1edbec43de0
BorjaSaavedra <72928428+BorjaSaavedra@users.noreply.github.com>
2023-11-13 12:38:41 +0100

    Create listener2.py para escuchar lvl bateria

---
+++ listener2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+import rospy
+from std_msgs.msg import String import BatteryStatus
+
+def callback(data):
+  rospy.loginfo("El nivel de la bateria es: " + data.level)
+def listener():
+  rospy.init_node(’listenerBateria’, anonymous=True)
+  rospy.Subscriber("/robot/battery_estimator/data", BatteryStatus, callback)
+  # spin() simply keeps python from exiting until this node is stopped
+  rospy.spin()
+if __name__ == ’__main__’:
+  listener()
