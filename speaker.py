b11258ce7204fa5ff5412f17f77e7c46664c3d3e
Riera <arierg03@estudiantes.unileon.es>
2023-12-06 16:15:34 +0100

    módulo altavoz general

---
+++ speaker.py
@@ -0,0 +1,51 @@
+import pyttsx3
+import rospy
+from std_msgs.msg import String
+from datetime import datetime  
+
+logs_path = 'say_logs.txt'
+
+#Declaraciones del altavoz
+engine = pyttsx3.init(driverName='espeak',debug=True)
+engine.setProperty('voice', 'es')
+engine.setProperty('volume', 1.0)
+
+#Funcionamiento
+def callback(data):
+    rospy.loginfo("Recibido: %s", data.data)
+    result = data.data
+    #Inicia la nueva reproduccion
+    engine.say(result)
+    engine.runAndWait()
+    #Publicamos el log
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    
+    # Construir la línea de log con [fecha - hora] mensaje leído
+    log_line = f"[{now}] {result}"
+    with open(logs_path, "a") as f:
+        f.write(log_line + "\n")
+
+    # Publicar el contenido del archivo en el tópico
+    with open(logs_path, "r") as f:
+        content = f.read()
+        log_publisher.publish(content)
+    
+
+def listener():
+    rospy.init_node('audio')
+    rospy.Subscriber('audio/say', String, callback)
+    rospy.spin()
+
+    #Variante de .spin()
+    # rate = rospy.Rate(10)  # Frecuencia de bucle (puedes ajustar según sea necesario)
+    
+    # while not rospy.is_shutdown():
+    #     rate.sleep()
+
+if __name__ == '__main__':
+    try:
+        #Inicializamos el publicador para el tópico de logs
+        log_publisher = rospy.Publisher('audio/say_logs', String, queue_size=10)
+        listener()
+    except rospy.ROSInterruptException:
+        pass
